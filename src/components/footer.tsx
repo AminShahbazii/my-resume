79584dcc50d83813c4e4805e4cc3077cd3abe804
Amin <mamadgonjishke@gmail.com>
2025-08-01 18:37:54 +0330

    fix somthing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer className="w-full h-[47px] border-t mt-12">
             <div className="max-w-[640px] mx-auto h-[47px]  px-6 sm:px-0 flex items-center justify-between text-sm opacity-75">
-                <p>I like simplicity, but let's not simplify it.</p>
+                <p>Virtu Vince Fortuna</p>
                 <div className="flex items-center gap-2.5">
                     <a
                         href="https://github.com/AminShahbazii"
@@ -17,7 +17,7 @@ export function Footer() {
                         />
                     </a>
                     <a
-                        href="https://t.me/IamAminSh"
+                        href="https://t.me/joqd_net"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="hover:opacity-100 opacity-75 transition-opacity"
@@ -28,7 +28,18 @@ export function Footer() {
                             className="w-4 h-4 dark:invert dark:brightness-200"
                         />
                     </a>
-                   
+                    <a
+                        href="https://x.com/IAmAminSh_"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:opacity-100 opacity-75 transition-opacity"
+                    >
+                        <img
+                            src="/icons/x.svg"
+                            alt="X"
+                            className="w-4 h-4 dark:invert dark:brightness-200"
+                        />
+                    </a>
                 </div>
             </div>
         </footer>
